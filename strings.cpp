df9241989ac29d3051d208eb2faaa87ffec97b1e
tyguybart <42874960+tyguybart@users.noreply.github.com>
2018-12-05 13:39:23 -0500

    git

--- strings.cpp
+++ strings.cpp
@@ -1,9 +1,1 @@
-#include "strings.h"
-#include <string>
-using std::string;
-
-const string Strings::SERVER_NAME = "tcp:customerapp-rrb.database.windows.net";
-const string Strings::DATABASE_NAME = "contactmanager-db";
-const string Strings::DATABASE_USER_ID = "access@customerapp-rrb";
-const string Strings::DATABASE_PASSWORD = "PhillyIsTrash69";
 
